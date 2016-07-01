97cdd5c6af3118ae5ba1b6dce5c54a85fbd32a78
Terry G Lorber <tgl@rideside.net>
2016-07-01 11:41:34 -0400

    Try a logging utility

--- jobs.rb
+++ jobs.rb
@@ -9,8 +9,16 @@ require 'twitter'
 
 class Jobs
 
+  @@log_file = File.open(File.join(File.dirname(__FILE__), "log/#{ENV['RACK_ENV']}.log"), 'a')
+  @@log_file.sync = true
+  def log(msg)
+    line = "[#{Time.now.strftime('%H:%M:%S')}] #{msg}\n"
+    @@log_file.write(line)
+    puts line
+  end
+
   def poll_and_record_requests
-    puts "START at #{Time.now.strftime('%Y%m%d %T')}"
+    log "START at #{Time.now.strftime('%Y%m%d %T')}"
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['CONSUMER_KEY']
       config.consumer_secret     = ENV['CONSUMER_SECRET']
@@ -20,17 +28,17 @@ class Jobs
 
     client.mentions_timeline(result_type: "recent").take(100).each do |tweet|
       if Request.first(:tweet_id => tweet.id).nil?
-        puts "INFO: Tweet #{tweet.id} -- #{tweet.text}"
+        log "INFO: Tweet #{tweet.id} -- #{tweet.text}"
         r = Request.create(:tweet_id => tweet.id, 
                            :tweet_text => tweet.text)
         r.search_queue = SearchQueue.create
 
         if !r.save
           r.errors.each do |err|
-            puts "ERROR: Request save #{err}"
+            log "ERROR: Request save #{err}"
           end
         else
-          puts "INFO: Request save #{r.id}"
+          log "INFO: Request save #{r.id}"
         end
       end
     end
@@ -41,7 +49,7 @@ class Jobs
                     :deleted.not => true).each do |record|
       r = Request.get(record.request_id)
 
-      puts "REQUEST [#{r.text}]"
+      log "REQUEST [#{r.text}]"
 
       m = Net::HTTP.start('kat.cr', :use_ssl => true) do |http|
         resp = http.get("/usearch/#{URI::encode(r.text)}/")
@@ -51,7 +59,7 @@ class Jobs
         /magnet:\?[^"]*/.match(l.to_s)
       end
 
-      puts "MAGNET #{m}"
+      log "MAGNET #{m}"
 
       ml = MagnetLink.create(:request => r, :link => m)
       ml.request = r
@@ -59,10 +67,10 @@ class Jobs
       
       if !ml.save
         ml.errors.each do |err|
-          puts "ERROR: MagnetLink save #{err}"
+          log "ERROR: MagnetLink save #{err}"
         end
       else
-        puts "INFO: MagnetLink save #{ml.id}"
+        log "INFO: MagnetLink save #{ml.id}"
       end
     end
 
@@ -83,10 +91,10 @@ class Jobs
     TransmissionQueue.all(:fields => [:id, :magnet_link_id],
                           :deleted.not => true).each do |record|
       m = MagnetLink.get(record.magnet_link_id)
-      puts "LINK #{m.id} #{m.link}"
+      log "LINK #{m.id} #{m.link}"
         
       t = transmission_api.create(m.link)
-      puts "TORRENT #{t}"
+      log "TORRENT #{t}"
 
       unless t.nil?
         torrent = Torrent.create(:transmission_id => t['id'],
@@ -95,10 +103,10 @@ class Jobs
         torrent.magnet_link = m
         if !torrent.save
           torrent.errors.each do |err|
-            puts "ERROR: Torrent save #{err}"
+            log "ERROR: Torrent save #{err}"
           end
         else
-          puts "INFO: Torrent save #{torrent.id}"
+          log "INFO: Torrent save #{torrent.id}"
         end
       end
     end
@@ -119,51 +127,51 @@ class Jobs
 
     Torrent.all(:deleted.not => true).each do |record|
       t = transmission_api.find(record.transmission_id)
-      puts "INFO: Torrent id #{record.transmission_id} #{t['percentDone'] unless t.nil?}"
+      log "INFO: Torrent id #{record.transmission_id} #{t['percentDone'] unless t.nil?}"
       if t && 1 == t['percentDone']
-        puts "INFO: Torrent #{t['id']} is done."
+        log "INFO: Torrent #{t['id']} is done."
         length = t['totalSize']
-        puts "LENGTH [#{length}]"
+        log "LENGTH [#{length}]"
         curr = 0
         index = 0
         while curr < length do
           track = File.join(ENV['TRANSMISSION_COMPLETED_DIR'],
                             t['files'][index]['name'])
           
-          puts "INFO: #{track}"
+          log "INFO: #{track}"
 
           begin
             fkey = Datafile.store_on_s3(open(track, "rb"))
 
             name = File.basename(t['files'][index]['name'])
 
-            puts "DEBUG: id #{t['id']}"
-            puts "DEBUG: torrent #{t['name']}"
-            puts "DEBUG: filename #{name}"
-            puts "DEBUG: fkey #{fkey}"
+            log "DEBUG: id #{t['id']}"
+            log "DEBUG: torrent #{t['name']}"
+            log "DEBUG: filename #{name}"
+            log "DEBUG: fkey #{fkey}"
 
             d = Datafile.create(:torrent_id   => t['id'],
                                 :torrent_name => t['name'],
                                 :file_name    => name,
                                 :s3_fkey      => fkey)
 
-            puts "DEBUG: done Datafile create"
+            log "DEBUG: done Datafile create"
 
             if !d.save
               d.errors.each do |err|
-                puts "ERROR Datafile save #{err}"
+                log "ERROR Datafile save #{err}"
               end
             else
-              puts "ERROR Datafile #{d.id} saved"
+              log "ERROR Datafile #{d.id} saved"
             end
           rescue => e
-            puts "ERROR: #{e}"
+            log "ERROR: #{e}"
           end
 
           curr = curr + t['files'][index]['length']
           index = index + 1
 
-          puts "DEBUG: Next is #{index} #{curr}"
+          log "DEBUG: Next is #{index} #{curr}"
         end
 
         transmission_api.destroy(record.transmission_id)
@@ -194,16 +202,16 @@ class Jobs
   
             if r2.to_regexp.match(datafile.file_name)
               if /(mp3|mp4|ogg|webm|wav)$/.match(datafile.file_name)
-                puts "DEBUG: #{request.id} #{request.text}"
-                puts "DEBUG: #{datafile.id}"
-                puts "??? #{datafile.file_name}"
+                log "DEBUG: #{request.id} #{request.text}"
+                log "DEBUG: #{datafile.id}"
+                log "??? #{datafile.file_name}"
 
                 track = Track.create(:request_id => request.id,
                                      :datafile_id => datafile.id)
                 
                 if !track.save
                   track.errors.each do |err|
-                    puts "ERR: Track save #{err}"
+                    log "ERR: Track save #{err}"
                   end
                 end
 
@@ -225,7 +233,7 @@ class Jobs
     end
 
     tracks.each do |track|
-      puts "DEBUG: #{track.id} #{track.request_id} #{track.datafile_id}"
+      log "DEBUG: #{track.id} #{track.request_id} #{track.datafile_id}"
       track.request.update(:matched => true)
       track.datafile.update(:matched => true)
 
