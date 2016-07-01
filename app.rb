fe48ef1beb39102764d2fe2cb7b9a0d73277a26f
Terry G Lorber <tgl@rideside.net>
2016-07-01 10:25:58 -0400

    Guard against an empty library

--- app.rb
+++ app.rb
@@ -8,26 +8,25 @@ class App < Sinatra::Base
   
   set :logging, Logger::DEBUG
 
-  get '/' do
-    
-    all = Track.all
-    @size = all.length
-    id = ((rand * 100).to_i % @size) + 1
-    puts "DEBUG: Get Track #{id}"
-    track = Track.get(id)
-    @datafile = track.datafile
+  get '/' do  
+    if (@size = Track.all.length) > 0
+      id = ((rand * 100).to_i % @size) + 1
+      puts "DEBUG: Get Track #{id}"
+      track = Track.get(id)
+      @datafile = track.datafile
 
-    @@client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end 
+      @@client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token        = ENV['ACCESS_TOKEN']
+        config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+      end 
 
-    @tweet = @@client.status(track.request.tweet_id)
+      @tweet = @@client.status(track.request.tweet_id)
 
-    puts "DEBUG: #{@tweet}"
-    puts "DEBUG: #{@tweet['id']}"
+      puts "DEBUG: #{@tweet}"
+      puts "DEBUG: #{@tweet['id']}"
+    end
 
     haml :index
   end
