367617a65a9007c77ed663a2c29d025507b9ad17
Terry G Lorber <tgl@rideside.net>
2016-12-30 18:26:39 +0000

    Play just one track at a time

--- app.rb
+++ app.rb
@@ -10,12 +10,9 @@ class App < Sinatra::Base
 
   get '/' do  
     if (@size = Track.all(:deleted => false).length) > 0
-      tracks = {}
-      (1..5).each do       
-        id = ((rand * 100).to_i % @size) + 1
-        puts "DEBUG: Get Track #{id}"
-        tracks[id] = Track.get(id)
-      end
+      id = ((rand * 100).to_i % @size) + 1
+      puts "DEBUG: Get Track #{id}"
+      track = Track.get(id)
       
       @@client ||= Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['CONSUMER_KEY']
@@ -23,18 +20,16 @@ class App < Sinatra::Base
         config.access_token        = ENV['ACCESS_TOKEN']
         config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
       end 
-
-      tracks.each do |t|
-        puts "XXXXX #{t} XXXXX"
-        @tweet = @@client.status(t[1].request.tweet_id)
-      end
-
-      @datafiles = tracks.collect{|t| t[1].datafile}
+    
+      puts "XXXXX #{track} XXXXX"
+      @tweet = @@client.status(track.request.tweet_id)
+      
+      @datafile = track.datafile
       
       puts "DEBUG: #{@tweet}"
       puts "DEBUG: #{@tweet.id}"
     end
-
+    
     haml :index
   end
 
