fd9972ebd6d0a955a299344ca14a5345a5dea8ca
Terry G Lorber <tgl@rideside.net>
2017-02-20 06:13:08 +0000

    Nothing recent

--- app.rb
+++ app.rb
@@ -19,25 +19,22 @@ class App < Sinatra::Base
         puts "DEBUG: Get Track #{id}"
         track = Track.get(tracks[id].id)
         puts "DEBUG: #{track.created_at}"
-        if track.created_at > (Date.today - 15)
-          @@client ||= Twitter::REST::Client.new do |config|
-            config.consumer_key        = ENV['CONSUMER_KEY']
-            config.consumer_secret     = ENV['CONSUMER_SECRET']
-            config.access_token        = ENV['ACCESS_TOKEN']
-            config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-          end 
-    
-          puts "XXXXX #{track} XXXXX"
 
-          @tweet = @@client.status(track.request.tweet_id)
+        @@client ||= Twitter::REST::Client.new do |config|
+          config.consumer_key        = ENV['CONSUMER_KEY']
+          config.consumer_secret     = ENV['CONSUMER_SECRET']
+          config.access_token        = ENV['ACCESS_TOKEN']
+          config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+        end 
+    
+        puts "XXXXX #{track} XXXXX"
+        @tweet = @@client.status(track.request.tweet_id)
       
-          @datafile = track.datafile
+        @datafile = track.datafile
       
-          puts "DEBUG: #{@tweet}"
-          puts "DEBUG: #{@tweet.id}"
-        else
-          track = nil
-        end
+        puts "DEBUG: #{@tweet}"
+        puts "DEBUG: #{@tweet.id}"
+
         puts "DEBUG: COUNT #{count}"
         count = count + 1
       end
